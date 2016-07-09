e475a8a6779b126a7e0f7b1db1996e78dfbb6e67
burraboc <info@onelinebugfix.com>
2016-07-09 16:13:52 -0700

    Convert game-component to ES6 style class

--- src/components/game-component.js
+++ src/components/game-component.js
@@ -1,200 +1,186 @@
-'use strict';
-
 import $ from 'jquery';
-import Model from '../models/game-state';
+import GameState from '../models/game-state';
 
-function GameComponent (config) {
-  let defaultConfig = {
-    canvasContainerSelector: '',
-    controlContainerSelector: '',
-    width: 700,
-    height: 400,
-    rectWidth: 10,
-    rectHeight: 10
-  };
+export default class GameComponent {
+  constructor (config) {
+    let defaultConfig = {
+      canvasContainerSelector: '',
+      controlContainerSelector: '',
+      width: 700,
+      height: 400,
+      rectWidth: 10,
+      rectHeight: 10
+    };
 
-  this.props = $.extend(defaultConfig, config);
+    this.props = $.extend(defaultConfig, config);
 
-  this.init();
-}
-
-let proto = GameComponent.prototype;
-
-/**
- * Initialize the canvas layering and model
- */
-proto.init = function () {
-  let $canvasContainer = $(this.props.canvasContainerSelector);
-
-  if ($canvasContainer.length !== 1) {
-    throw new Error('The element doesn\'t exist');
+    this.init();
   }
 
-  this.$canvasContainer = $canvasContainer;
-  this.$canvasContainer.width(this.props.width);
-  this.$canvasContainer.height(this.props.height);
+  init () {
+    let $canvasContainer = $(this.props.canvasContainerSelector);
 
-  this.$controls = $(this.props.controlContainerSelector);
-
-  this.model = new Model({
-    x: this.props.width / this.props.rectWidth,
-    y: this.props.height / this.props.rectHeight
-  });
-
-  this.setupCanvas();
-  this.setCanvasEvents();
-  this.setControlEvents();
-};
-
-/**
- * Setup the gridCanvas, selectCanvas, and highlightCanvas layering
- */
-proto.setupCanvas = function () {
-  let self = this,
-    cxt, x, y, offset = 0;
-
-  ['gridCanvas', 'selectCanvas', 'highlightCanvas'].forEach(function (canvasName, idx) {
-    let canvasEl = self[canvasName] = document.createElement('canvas');
-
-    canvasEl.width = self.props.width;
-    canvasEl.height = self.props.height;
-    canvasEl.style.position = 'absolute';
-    canvasEl.style.left = 0;
-    canvasEl.style.top = 0;
-    canvasEl.style.zIndex = idx;
-
-    self.$canvasContainer.append(canvasEl);
-  });
-
-  cxt = this.gridCanvas.getContext('2d');
-
-  for (x = 0; x <= this.props.width; x += this.props.rectWidth) {
-    cxt.moveTo(x + offset, offset);
-    cxt.lineTo(x + offset, this.props.height + offset);
-  }
-
-  for (y = 0; y <= this.props.height; y += this.props.rectHeight) {
-    cxt.moveTo(offset, y + offset);
-    cxt.lineTo(this.props.width + offset, y + offset);
-  }
-
-  cxt.strokeStyle = 'black';
-  cxt.stroke();
-};
-
-/**
- * Set listeners for highlighting and selection
- */
-proto.setCanvasEvents = function () {
-  let self = this,
-    hightlightCxt = self.highlightCanvas.getContext('2d'),
-    selectCxt = self.selectCanvas.getContext('2d'),
-    rWidth = self.props.rectWidth,
-    rHeight = self.props.rectHeight,
-    oldRect;
-
-  let clearOldRect = function () {
-    if (oldRect) {
-      hightlightCxt.clearRect(oldRect.xPos, oldRect.yPos, rWidth, rHeight);
+    if ($canvasContainer.length !== 1) {
+      throw new Error('The element does not exist or should only target one dom element');
     }
-  };
 
-  let highlightArea = function (coor) {
-    clearOldRect();
+    this.$canvasContainer = $canvasContainer;
+    this.$canvasContainer.width(this.props.width);
+    this.$canvasContainer.height(this.props.height);
 
-    hightlightCxt.fillStyle = '#7ec0ee';
-    hightlightCxt.fillRect(coor.xPos, coor.yPos, rWidth, rHeight);
+    this.$controls = $(this.props.controlContainerSelector);
 
-    oldRect = coor;
-  };
+    this.model = new GameState({
+      x: this.props.width / this.props.rectWidth,
+      y: this.props.height / this.props.rectHeight
+    });
 
-  let toggleSelectArea = function (coor) {
-    self.model.toggleAreaState(coor.xNum, coor.yNum);
+    this.setupCanvas();
+    this.setCanvasEvents();
+    this.setControlEvents();
+  }
 
-    let currentState = self.model.getAreaState(coor.xNum, coor.yNum);
+  setupCanvas () {
+    const self = this;
 
-    if (currentState) {
-      selectCxt.fillStyle = '#000080';
-      selectCxt.fillRect(coor.xPos, coor.yPos, rWidth, rHeight);
-    } else {
-      selectCxt.clearRect(coor.xPos, coor.yPos, rWidth, rHeight);
+    ['gridCanvas', 'selectCanvas', 'highlightCanvas'].forEach((canvasName, idx) => {
+      let canvasEl = self[canvasName] = document.createElement('canvas');
+
+      canvasEl.width = self.props.width;
+      canvasEl.height = self.props.height;
+      canvasEl.style.position = 'absolute';
+      canvasEl.style.left = 0;
+      canvasEl.style.top = 0;
+      canvasEl.style.zIndex = idx;
+
+      self.$canvasContainer.append(canvasEl);
+    });
+
+    let cxt = this.gridCanvas.getContext('2d');
+    let offset = 0;
+
+    for (let x = 0; x <= this.props.width; x += this.props.rectWidth) {
+      cxt.moveTo(x + offset, offset);
+      cxt.lineTo(x + offset, this.props.height + offset);
     }
-  };
 
-  let updateSelectArea = function (grid) {
-    for (let x = 0; x < grid.length; x++) {
-      for (let y = 0; y < grid[0].length; y++) {
-        if (grid[x][y]) {
-          selectCxt.fillStyle = '#000080';
-          selectCxt.fillRect(x * rWidth, y * rHeight, rWidth, rHeight);
-        } else {
-          selectCxt.clearRect(x * rWidth, y * rHeight, rWidth, rHeight);
+    for (let y = 0; y <= this.props.height; y += this.props.rectHeight) {
+      cxt.moveTo(offset, y + offset);
+      cxt.lineTo(this.props.width + offset, y + offset);
+    }
+
+    cxt.strokeStyle = 'black';
+    cxt.stroke();
+  }
+
+  setCanvasEvents () {
+    const self = this;
+
+    let highlightCxt = self.highlightCanvas.getContext('2d');
+    let selectCxt = self.selectCanvas.getContext('2d');
+    let rWidth = self.props.rectWidth;
+    let rHeight = self.props.rectHeight;
+    let oldRect;
+
+    function clearOldRect () {
+      if (oldRect) {
+        highlightCxt.clearRect(oldRect.xPos, oldRect.yPos, rWidth, rHeight);
+      }
+    }
+
+    function highlightArea (coor) {
+      clearOldRect();
+
+      highlightCxt.fillStyle = '#7ec0ee';
+      highlightCxt.fillRect(coor.xPos, coor.yPos, rWidth, rHeight);
+
+      oldRect = coor;
+    }
+
+    function toggleSelectArea (coor) {
+      self.model.toggleAreaState(coor.xNum, coor.yNum);
+
+      let currentState = self.model.getAreaState(coor.xNum, coor.yNum);
+
+      if (currentState) {
+        selectCxt.fillStyle = '#000080';
+        selectCxt.fillRect(coor.xPos, coor.yPos, rWidth, rHeight);
+      } else {
+        selectCxt.clearRect(coor.xPos, coor.yPos, rWidth, rHeight);
+      }
+    }
+
+    function updateSelectArea (grid) {
+      for (let x = 0; x < grid.length; x++) {
+        for (let y = 0; y < grid[0].length; y++) {
+          if (grid[x][y]) {
+            selectCxt.fillStyle = '#000080';
+            selectCxt.fillRect(x * rWidth, y * rHeight, rWidth, rHeight);
+          } else {
+            selectCxt.clearRect(x * rWidth, y * rHeight, rWidth, rHeight);
+          }
         }
       }
     }
-  };
 
-  let getPosition = function (evt) {
-    let rect = self.gridCanvas.getBoundingClientRect(),
-      x = evt.clientX - rect.left,
-      y = evt.clientY - rect.top,
-      xNum = Math.floor(x / rWidth),
-      yNum = Math.floor(y / rHeight);
+    function getPosition (evt) {
+      let rect = self.gridCanvas.getBoundingClientRect();
+      let x = evt.clientX - rect.left;
+      let y = evt.clientY - rect.top;
+      let xNum = Math.floor(x / rWidth);
+      let yNum = Math.floor(y / rHeight);
 
-    return {
-      xNum: Math.abs(xNum),
-      yNum: Math.abs(yNum),
-      xPos: xNum * rWidth,
-      yPos: yNum * rHeight
-    };
-  };
+      return {
+        xNum: Math.abs(xNum),
+        yNum: Math.abs(yNum),
+        xPos: xNum * rWidth,
+        yPos: yNum * rHeight
+      };
+    }
 
-  $(this.highlightCanvas).mousemove(function (evt) {
-    highlightArea(getPosition(evt));
-  });
+    $(this.highlightCanvas).mousemove((evt) => {
+      highlightArea(getPosition(evt));
+    });
 
-  $(this.highlightCanvas).mouseout(function () {
-    clearOldRect();
-  });
+    $(this.highlightCanvas).mouseout(() => {
+      clearOldRect();
+    });
 
-  $(this.highlightCanvas).click(function (evt) {
-    toggleSelectArea(getPosition(evt));
-  });
+    $(this.highlightCanvas).click((evt) => {
+      toggleSelectArea(getPosition(evt));
+    });
 
-  self.model.onAreaStateUpdate(function (grid) {
-    updateSelectArea(grid);
-  });
-};
+    self.model.onAreaStateUpdate((grid) => {
+      updateSelectArea(grid);
+    });
+  }
 
-/**
- * Set listeners for the controls
- */
-proto.setControlEvents = function () {
-  let self = this,
-    $play = this.$controls.find('.play'),
-    $pause = this.$controls.find('.pause');
+  setControlEvents () {
+    const self = this;
 
-  $play.click(function () {
-    $play.prop('disabled', true);
-    $pause.prop('disabled', false);
+    let $play = this.$controls.find('.play');
+    let $pause = this.$controls.find('.pause');
 
-    self.model.play();
-  });
+    $play.click(() => {
+      $play.prop('disabled', true);
+      $pause.prop('disabled', false);
 
-  $pause.click(function () {
-    $play.prop('disabled', false);
-    $pause.prop('disabled', true);
+      self.model.play();
+    });
 
-    self.model.pause();
-  });
+    $pause.click(() => {
+      $play.prop('disabled', false);
+      $pause.prop('disabled', true);
 
-  this.$controls.find('.step-forward').click(function () {
-    self.model.stepForward();
-  });
+      self.model.pause();
+    });
 
-  this.$controls.find('.reset').click(function () {
-    self.model.reset();
-  });
-};
+    this.$controls.find('.step-forward').click(() => {
+      self.model.stepForward();
+    });
 
-export default GameComponent;
+    this.$controls.find('.reset').click(() => {
+      self.model.reset();
+    });
+  }
+}
